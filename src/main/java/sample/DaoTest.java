8698768f6a4f9293da9fc45b92716a8e1160b5b6
yjzhao <630877986@qq.com>
2020-06-04 19:48:10 +0800

    小修改

--- src/main/java/sample/DaoTest.java
+++ src/main/java/sample/DaoTest.java
@@ -25,18 +25,19 @@ public class DaoTest {
         UserDao tool = new UserDaoImpl();
 
 //        List<Msg> list = tool.getMsg("Prodigal_son","Elio");
-        tool.setMsgIsRead("Prodigal_son","Peter");
-//        User user = tool.getUserByUserName_Password("Prodigal_son","zyj19981027");
+       // tool.setMsgIsRead("Prodigal_son","Peter");
+        User user = tool.getUserByUserName_Password("Prodigal_son","123456");
 //        List<User> list = tool.getAllFriends("Prodigal_son");
 //        System.out.println(list.get(0).getMsg());
 //        System.out.println(list.get(1).getMsg());
-//        System.out.println(user.getUserName());
-//        System.out.println(user.getPassword());
-//        System.out.println(user.getSex());
-//        System.out.println(user.getMotto());
-//        System.out.println(user.getAge());
-//        System.out.println(user.getUserId());
-//        System.out.println(user.getOnline());
+        System.out.println(user.getUserName());
+        System.out.println(user.getPassword());
+        System.out.println(user.getSex());
+        System.out.println(user.getMotto());
+        System.out.println(user.getAge());
+        System.out.println(user.getUserId());
+        System.out.println(user.getOnline());
+        System.out.println(user.getHeadImg());
     }
 }
 
