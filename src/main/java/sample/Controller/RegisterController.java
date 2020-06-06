77900fefefbc4158448e1fe34e46faf48f7dc826
yjzhao <630877986@qq.com>
2020-06-06 18:54:43 +0800

    去掉无用的部分

---
+++ src/main/java/sample/Controller/RegisterController.java
@@ -0,0 +1,97 @@
+package sample.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import sample.Dao.UserDao;
+import sample.Dao.UserDaoImpl;
+import sample.Entity.User;
+
+
+import java.beans.EventHandler;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class RegisterController implements Initializable {
+    @FXML
+    private Pane registerPane;
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField comfirmPassword;
+    @FXML
+    private RadioButton maleButton;
+    @FXML
+    private RadioButton femaleButton;
+    @FXML
+    private DatePicker birthday;
+    @FXML
+    private TextField motto;
+
+    private UserDao dao;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        dao = new UserDaoImpl();
+    }
+
+    public void maleButtonAction(ActionEvent actionEvent) {
+        maleButton.setSelected(true);
+        femaleButton.setSelected(false);
+    }
+
+    public void femaleButtonAction(ActionEvent actionEvent) {
+        maleButton.setSelected(false);
+        femaleButton.setSelected(true);
+    }
+
+    public void register_buttonAction(ActionEvent actionEvent) throws ParseException {
+        String userName = this.userName.getText();
+        String password = this.password.getText();
+        String comfirmPassword = this.comfirmPassword.getText();
+        String sex = maleButton.isSelected() ? "male" : "female";
+        Date birthday = this.birthday.getValue() != null ? new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(this.birthday.getValue())) : null;
+        String motto = this.motto.getText();
+        //  信息不完整
+        if (userName.equals("") || password.equals("") || comfirmPassword.equals("")
+        || sex.equals("") || birthday == null || motto.equals("")){
+            Stage stage = new Stage();
+            Label l = new Label("信息不能为空!");
+            Scene s = new Scene(l,200,124);
+            stage.setScene(s);
+            stage.show();
+            return;
+        }
+        //  用户名已存在
+        if (dao.getUserByUserName(userName) != null){
+            Stage stage = new Stage();
+            Label l = new Label("用户名已存在!");
+            Scene s = new Scene(l,200,124);
+            stage.setScene(s);
+            stage.show();
+            return;
+        }
+        //  密码和确认密码不同
+        if (!password.equals(comfirmPassword)){
+            Stage stage = new Stage();
+            Label l = new Label("密码两次输入不同!");
+            Scene s = new Scene(l,200,124);
+            stage.setScene(s);
+            stage.show();
+            return;
+        }
+        User user = new User("1",userName,password,sex,motto,birthday,false,null);
+        dao.registerUser(user);
+        Stage stage = (Stage) registerPane.getScene().getWindow();
+        stage.close();
+    }
+}
--- src/main/java/sample/Util/ImgUtil.java
+++ src/main/java/sample/Util/ImgUtil.java
@@ -2,8 +2,6 @@ package sample.Util;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
-import sample.Dao.UserDao;
-import sample.Dao.UserDaoImpl;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
@@ -25,6 +23,7 @@ public class ImgUtil {
         }
         return encoder.encodeBuffer(imageBytes).trim();
     }
+    //  Base64字符串转化为图片
     public Image base64toImage(String str) throws IOException {
         BASE64Decoder decoder = new BASE64Decoder();
         byte[] bytes = decoder.decodeBuffer(str);
@@ -32,36 +31,4 @@ public class ImgUtil {
         BufferedImage bi1 =ImageIO.read(stream);
         return SwingFXUtils.toFXImage(bi1,null);
     }
-    public static void main(String[] args) throws IOException {
-        //图片转化为二进制
-        byte[] imageBytes = null;
-        try  {
-            FileInputStream fileInputStream = new FileInputStream(new File("/Users/zhaoyinjie/Desktop/head1.jpg"));
-            imageBytes = new byte[fileInputStream.available()];
-            fileInputStream.read(imageBytes);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BASE64Encoder encoder = new BASE64Encoder();
-        BASE64Decoder decoder = new BASE64Decoder();
-        String str = encoder.encodeBuffer(imageBytes).trim();
-        UserDao dao = new UserDaoImpl();
-        dao.test(str);
-//        String str1 = dao.test1();
-//        System.out.println(str1);
-//        byte[] bytes = decoder.decodeBuffer(str1);
-//        //二进制转化为图片
-//        try  {
-//            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-//            BufferedImage bi1 =ImageIO.read(bais);
-//            Image image = SwingFXUtils.toFXImage(bi1,null);
-//            System.out.println(image);
-////            FileOutputStream fileOutputStream = new FileOutputStream(new File("/Users/zhaoyinjie/Desktop/head1.jpg"));
-////            fileOutputStream.write(bytes);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-
 }
